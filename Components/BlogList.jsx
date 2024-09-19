29bbbaa2a738cbcb2d3c2d9f25aa56de0ccf95d4
Rashed Al-Masri <157921405+BloodyFlakes@users.noreply.github.com>
2024-09-19 19:30:26 +0300

    Add the footer

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -1,25 +1,51 @@
 import { blog_data } from '@/Assets/assets';
 import BlogItem from './BlogItem';
+import { useState } from 'react';
 
 function BlogList() {
+  const [menu, setMenu] = useState('All');
+
   return (
     <div>
       <div className='flex justify-center gap-6 my-10'>
-        <button className='px-4 py-1 text-white bg-black rounded-sm'>All</button>
-        <button>Technology</button>
-        <button>Startup</button>
-        <button>Lifestyle</button>
+        <button
+          onClick={() => setMenu('All')}
+          className={menu === 'All' ? 'px-4 py-1 text-white bg-black rounded-sm' : ''}
+        >
+          All
+        </button>
+        <button
+          onClick={() => setMenu('Technology')}
+          className={menu === 'Technology' ? 'px-4 py-1 text-white bg-black rounded-sm' : ''}
+        >
+          Technology
+        </button>
+        <button
+          onClick={() => setMenu('Startup')}
+          className={menu === 'Startup' ? 'px-4 py-1 text-white bg-black rounded-sm' : ''}
+        >
+          Startup
+        </button>
+        <button
+          onClick={() => setMenu('Lifestyle')}
+          className={menu === 'Lifestyle' ? 'px-4 py-1 text-white bg-black rounded-sm' : ''}
+        >
+          Lifestyle
+        </button>
       </div>
+
       <div className='flex flex-wrap justify-around gap-1 mb-16 gap-y-10 xl:mx-24'>
-        {blog_data.map((item, index) => (
-          <BlogItem
-            title={item.title}
-            category={item.category}
-            description={item.description}
-            image={item.image}
-            key={index}
-          />
-        ))}
+        {blog_data
+          .filter((blog) => (menu === 'All' ? true : blog.category === menu))
+          .map((item, index) => (
+            <BlogItem
+              title={item.title}
+              category={item.category}
+              description={item.description}
+              image={item.image}
+              key={index}
+            />
+          ))}
       </div>
     </div>
   );
